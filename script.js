6e1ce0e8c6d5c627a6163b6a98a060773594bdbd
Glitch (glitch-blank-website) <none>
2022-03-26 21:57:13 +0000

    🐿🛢 Checkpoint ./style.css:43335988/235 ./index.html:43335988/31 ./script.js:43335988/516

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
     to your site with Javascript */
 
 console.log("Hello, world!");
+let pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+let progress = 0; 
+let gamePlaying = false;
+
+let startGame = () => {
+  gamePlaying = true;
+  progress = 0;
+  document.getElementById("startButton").classList.add("hidden")
+  document.getElementById("endButton").classList.remove("hidden")
+}
+
+let endGame = () => {
+  gamePlaying = end;
+  
+}
