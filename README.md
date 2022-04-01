4a8e691b60de99ab247e18ba936d68587b9c3e8f
Glitch (glitch-blank-website) <none>
2022-03-31 22:28:32 -0400

    Updated README with responses to prework prompts and updated script.js to match what was on the glitch project.

--- README.md
+++ README.md
@@ -1,22 +1,87 @@
-# Hello Website (blank)
+# Pre-work - *Memory Game*
 
-This is a basic static HTML starter project you can build on however you like. No need to save. While you develop your site, your changes will happen 🪄 immediately in the preview window. You can upload assets like images or audio in `assets` to the left. The rest is up to you and your imagination.
+**Memory Game** is a Light & Sound Memory game to apply for CodePath's SITE Program. 
 
-## What's in this project?
+Submitted by: Sabahat Sami
 
-← `README.md`: That's this file, where you can tell people what your cool website does and how you built it.
+Time spent: 5 hours spent in total
 
-← `index.html`: This is the main HTML page for your site.
+Link to project: (https://glitch.com/edit/#!/artistic-shore-coral)
 
-← `style.css`: CSS files add styling rules to your content.
+## Required Functionality
 
-← `script.js`: If you're feeling fancy you can add interactivity to your site with JavaScript.
+The following **required** functionality is complete:
 
-![Glitch](https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576)
+* [1] Game interface has a heading (h1 tag), a line of body text (p tag), and four buttons that match the demo app
+* [1] "Start" button toggles between "Start" and "Stop" when clicked. 
+* [1] Game buttons each light up and play a sound when clicked. 
+* [1] Computer plays back sequence of clues including sound and visual cue for each button
+* [1] Play progresses to the next turn (the user gets the next step in the pattern) after a correct guess. 
+* [1] User wins the game after guessing a complete pattern
+* [1] User loses the game after an incorrect guess
 
-## You built this with Glitch!
+The following **optional** features are implemented:
 
-[Glitch](https://glitch.com) is a friendly community where millions of people come together to build web apps and websites.
+* [1] Any HTML page elements (including game buttons) has been styled differently than in the tutorial
+* [ ] Buttons use a pitch (frequency) other than the ones in the tutorial
+* [1] More than 4 functional game buttons
+* [ ] Playback speeds up on each turn
+* [1] Computer picks a different pattern each time the game is played
+* [1] Player only loses after 3 mistakes (instead of on the first mistake)
+* [ ] Game button appearance change goes beyond color (e.g. add an image)
+* [ ] Game button sound is more complex than a single tone (e.g. an audio file, a chord, a sequence of multiple tones)
+* [ ] User has a limited amount of time to enter their guess on each turn
 
-- Need more help? [Check out our Help Center](https://help.glitch.com/) for answers to any common questions.
-- Ready to make it official? [Become a paid Glitch member](https://glitch.com/pricing) to boost your app with private sharing, more storage and memory, domains and more.
+The following **additional** features are implemented:
+
+- [ ] List anything else that you can get done to improve the app!
+
+## Video Walkthrough (GIF)
+
+If you recorded multiple GIFs for all the implemented features, you can add them here:
+![](https://i.imgur.com/VWcDDVL.gif)
+
+![](https://i.imgur.com/UapLy2l.gif)
+
+![](gif3-link-here)
+![](gif4-link-here)
+
+## Reflection Questions
+1. If you used any outside resources to help complete your submission (websites, books, people, etc) list them here. 
+I used websites like W3Wschools to learn more about selecting elements using the dom. 
+
+2. What was a challenge you encountered in creating this submission (be specific)? How did you overcome it? (recommended 200 - 400 words) 
+A challenge that I encountered in creating this submission was trying to debug my project. When I used the inspect tools, the console would not show any of the messages I was console logging. This proved especially troublesome when I was trying to implement the feature that would let the buttons light up on their own without clicking on the buttons on myself. The buttons would not light up and I spent a good 30 minutes trying to figure out why it was not working until I realized there was a typo in one of my class names. Another challenge I ran into was playing the sequence of clues. The for loop that I used originally was for(let i = 0; i < progress; i++) However, since progress was initially set to 0 the for loop would never run because the condition would immediately evaluate to false. To fix this I changed the for loop to for(let i = 0; i < progress + 1; i++) in order to account for this. Another way would be to do for(let i = 0; i <= progress; i++). Another challenge I ran into was ending the game properly. The sequence would play an extra time at the end even after I guessed right because I checked for the winning condition before I incremented my progress instead of checking afterwards. There were no major challenges in this project but just a lot of small little problems that would arise that would take me some time to figure out and solve. 
+
+
+
+3. What questions about web development do you have after completing your submission? (recommended 100 - 300 words) 
+A question I have about web development now is that since this project was front-end based using javascript, HTML, and CSS, what does the backend side of web development look like? Something that I thought about as I was working on the project was it would be an interesting idea to have some sort of database that would store stats such as high scores and guess accuracy but was not sure how I would implement that. I understand that the backend is involved with communicating with the server and interacting with the database, sending necessary data to the front-end side while also processing data. However, how can we connect the server side and the front end side together? I believe that after this project, I’d like to learn more about the backend part of web development.
+
+
+4. If you had a few more hours to work on this project, what would you spend them doing (for example: refactoring certain functions, adding additional features, etc). Be specific. (recommended 100 - 300 words) 
+One thing that I would like to implement if I had more time would be to do the rest of the optional features. One optional feature in particular I would have liked to implement would have been the ticking clock feature. I’m not sure on how I would approach implementing something like that, but if given a few more hours I would have definitely looked more into it. Another feature that I would have liked to implement would have been storing information from past games and displaying that information to the user. I think in order to do something like that I would have to look into how information is stored and can be retrieved, something that I would need more time to figure out how to implement. 
+
+
+
+
+## Interview Recording URL Link
+
+[My 5-minute Interview Recording](your-link-here)
+
+
+## License
+
+    Copyright [YOUR NAME]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- script.js
+++ script.js
@@ -1,171 +1,171 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
-const clueHoldTime = 1000;
-const cluePauseTime = 333;
-const nextClueWaitTime = 1000;
-console.log("Hello, world!");
-
-
-let colorbtn = {
-  1:"red",
-  2:"green",
-  3:"magenta",
-  4:"yellow",
-  5:"blue"
-}
-
-let progress = 0; 
-let gamePlaying = false;
-let tonePlaying = false;
-let volume = 0.5;  
-let guessCounter = 0;
-let NUM_OF_BUTTONS = 5;
-let strikes = 0;
-let pattern = []
-let STRIKES_ALLOWED = 3;
-
-let strike = document.getElementById("strikes");
-let newStrike = document.createTextNode("strikes: " + strikes);
-strike.appendChild(newStrike);
-
-let updateStrikes = (strikes) => {
-  strike.innerHTML = "strikes: " + strikes;
-}
-
-let generateRandomPattern = () => {
-  let pattern = []
-  let randLength = Math.floor(Math.random() * 6 + 2)
-  for(let i = 0; i < randLength; i++){
-    pattern.push(Math.floor(Math.random() * (NUM_OF_BUTTONS) + 1))
-  }
-  return pattern;
-}
-
-let startGame = () => {
-  gamePlaying = true;
-  progress = 0;
-  pattern = generateRandomPattern();
-  alert(pattern);
-  document.getElementById("startButton").classList.add("hidden")
-  document.getElementById("endButton").classList.remove("hidden")
-  playClueSequence();
-
-}
-
-let endGame = () => {
-  gamePlaying = false;
-  document.getElementById("endButton").classList.add("hidden")
-  document.getElementById("startButton").classList.remove("hidden")
-  updateStrikes(0);
-}
-
-function lightButton(color){
-  console.log(color+"Square");
-  document.getElementById(color+"Square").classList.add("lit")
-}
-
-function clearButton(color){
-  document.getElementById(color+"Square").classList.remove("lit")
-}
-
-function playSingleClue(btn){
-  let color = colorbtn[btn];
-  lightButton(color);
-  playTone(btn, clueHoldTime);
-  setTimeout(clearButton, clueHoldTime, color)
-}
-
-let playClueSequence = () => {
-  guessCounter = 0;
-  let delay = nextClueWaitTime;
-  for(let i = 0; i < progress+1; i++){
-    setTimeout(playSingleClue, delay, pattern[i]);
-    delay += clueHoldTime;
-    delay += cluePauseTime;
-  }
-}
-
-let loseGame = () => {
-  endGame();
-  alert("You Lost!")
-}
-
-let winGame = () => {
-  endGame();
-  alert("You Won!")
-}
-
-let guess = (btn) => {
-  if(!gamePlaying){
-    return;
-  }
-  
-  if(btn === pattern[guessCounter]){
-    if(guessCounter !== progress){
-      guessCounter += 1
+    const clueHoldTime = 1000;
+    const cluePauseTime = 333;
+    const nextClueWaitTime = 1000;
+    console.log("Hello, world!");
+    
+    
+    let colorbtn = {
+      1:"red",
+      2:"green",
+      3:"magenta",
+      4:"yellow",
+      5:"blue"
     }
-    else{
-        progress += 1
-        if(progress === pattern.length){
-          winGame();
+    
+    let progress = 0; 
+    let gamePlaying = false;
+    let tonePlaying = false;
+    let volume = 0.5;  
+    let guessCounter = 0;
+    let NUM_OF_BUTTONS = 5;
+    let strikes = 0;
+    let pattern = []
+    let STRIKES_ALLOWED = 3;
+    
+    let strike = document.getElementById("strikes");
+    let newStrike = document.createTextNode("strikes: " + strikes);
+    strike.appendChild(newStrike);
+    
+    let updateStrikes = (strikes) => {
+      strike.innerHTML = "strikes: " + strikes;
+    }
+    
+    let generateRandomPattern = () => {
+      let pattern = []
+      let randLength = Math.floor(Math.random() * 6 + 2)
+      for(let i = 0; i < randLength; i++){
+        pattern.push(Math.floor(Math.random() * (NUM_OF_BUTTONS) + 1))
+      }
+      return pattern;
+    }
+    
+    let startGame = () => {
+      gamePlaying = true;
+      progress = 0;
+      pattern = generateRandomPattern();
+      document.getElementById("startButton").classList.add("hidden")
+      document.getElementById("endButton").classList.remove("hidden")
+      playClueSequence();
+    
+    }
+    
+    let endGame = () => {
+      gamePlaying = false;
+      document.getElementById("endButton").classList.add("hidden")
+      document.getElementById("startButton").classList.remove("hidden")
+      strikes = 0;
+      updateStrikes(0);
+    }
+    
+    function lightButton(color){
+      console.log(color+"Square");
+      document.getElementById(color+"Square").classList.add("lit")
+    }
+    
+    function clearButton(color){
+      document.getElementById(color+"Square").classList.remove("lit")
+    }
+    
+    function playSingleClue(btn){
+      let color = colorbtn[btn];
+      lightButton(color);
+      playTone(btn, clueHoldTime);
+      setTimeout(clearButton, clueHoldTime, color)
+    }
+    
+    let playClueSequence = () => {
+      guessCounter = 0;
+      let delay = nextClueWaitTime;
+      for(let i = 0; i < progress+1; i++){
+        setTimeout(playSingleClue, delay, pattern[i]);
+        delay += clueHoldTime;
+        delay += cluePauseTime;
+      }
+    }
+    
+    let loseGame = () => {
+      endGame();
+      alert("You Lost!")
+    }
+    
+    let winGame = () => {
+      endGame();
+      alert("You Won!")
+    }
+    
+    let guess = (btn) => {
+      if(!gamePlaying){
+        return;
+      }
+      
+      if(btn === pattern[guessCounter]){
+        if(guessCounter !== progress){
+          guessCounter += 1
         }
         else{
-          playClueSequence()
+            progress += 1
+            if(progress === pattern.length){
+              winGame();
+            }
+            else{
+              playClueSequence()
+            }
+            }
+          } 
+      else {
+        strikes += 1
+        updateStrikes(strikes);
+        if(strikes === STRIKES_ALLOWED){
+          strikes = 0;
+          loseGame();
+          }
+        else{
+          playClueSequence();
         }
-        }
-      } 
-  else {
-    strikes += 1
-    updateStrikes(strikes);
-    if(strikes === STRIKES_ALLOWED){
-      strikes = 0;
-      loseGame();
       }
-    else{
-      playClueSequence();
     }
-  }
-}
-
-
-// Sound Synthesis Functions
-const freqMap = {
-  1: 261.6,
-  2: 329.6,
-  3: 392,
-  4: 466.2,
-  5: 300.2
-}
-function playTone(btn,len){ 
-  o.frequency.value = freqMap[btn]
-  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-  context.resume()
-  tonePlaying = true
-  setTimeout(function(){
-    stopTone()
-  },len)
-}
-function startTone(btn){
-  if(!tonePlaying){
-    context.resume()
-    o.frequency.value = freqMap[btn]
-    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-    context.resume()
-    tonePlaying = true
-  }
-}
-function stopTone(){
-  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
-  tonePlaying = false
-}
-
-// Page Initialization
-// Init Sound Synthesizer
-var AudioContext = window.AudioContext || window.webkitAudioContext 
-var context = new AudioContext()
-var o = context.createOscillator()
-var g = context.createGain()
-g.connect(context.destination)
-g.gain.setValueAtTime(0,context.currentTime)
-o.connect(g)
-o.start(0)
+    
+    
+    // Sound Synthesis Functions
+    const freqMap = {
+      1: 261.6,
+      2: 329.6,
+      3: 392,
+      4: 466.2,
+      5: 300.2
+    }
+    function playTone(btn,len){ 
+      o.frequency.value = freqMap[btn]
+      g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+      context.resume()
+      tonePlaying = true
+      setTimeout(function(){
+        stopTone()
+      },len)
+    }
+    function startTone(btn){
+      if(!tonePlaying){
+        context.resume()
+        o.frequency.value = freqMap[btn]
+        g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+        context.resume()
+        tonePlaying = true
+      }
+    }
+    function stopTone(){
+      g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+      tonePlaying = false
+    }
+    
+    // Page Initialization
+    // Init Sound Synthesizer
+    var AudioContext = window.AudioContext || window.webkitAudioContext 
+    var context = new AudioContext()
+    var o = context.createOscillator()
+    var g = context.createGain()
+    g.connect(context.destination)
+    g.gain.setValueAtTime(0,context.currentTime)
+    o.connect(g)
+    o.start(0)
